43d94bc26340467a181497c5de8366ecfa10aee6
Saarmets <ralf.saarmets@gmail.com>
2024-07-02 20:12:10 +0300

    prankster strafemees

--- Main.java
+++ Main.java
@@ -165,7 +165,7 @@ public class Main extends LinearOpMode{
 
                 // drive using manual POV Joystick mode.  Slow things down to make the robot more controlable.
                 drive  = -gamepad1.left_stick_y  / 2.0;  // Reduce drive rate to 50%.
-                strafe = -gamepad1.left_stick_x  / 2.0;  // Reduce strafe rate to 50%.
+                strafe = gamepad1.left_stick_x  / 2.0;  // Reduce strafe rate to 50%.
                 turn   = -gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
                 telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
             }
@@ -189,7 +189,7 @@ public class Main extends LinearOpMode{
             }
 
             // Apply desired axes motions to the drivetrain.
-            moveRobot(drive, -strafe, turn);
+            moveRobot(drive, strafe, turn);
             sleep(10);
         }
     }
