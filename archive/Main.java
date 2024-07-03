adfe3aecc9ba58ec7afd22de526cbf84418434b0
Krihvik <partel.kukk@gmail.com>
2024-07-03 12:34:07 +0300

    organisation

--- archive/Main.java
+++ archive/Main.java
@@ -1,5 +1,5 @@
 
-package org.firstinspires.ftc.teamcode;
+package archive;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
