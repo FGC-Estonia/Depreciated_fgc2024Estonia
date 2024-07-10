a3bc3f80ec3ba3ba5fcfc6a6ed99817fc874cda8
Saarmets <65457878+saarmets@users.noreply.github.com>
2024-07-10 15:12:26 +0300

    IMU INITALIZE

--- MoveRobot.java
+++ MoveRobot.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class MoveRobot{
     private DcMotorEx leftFrontDriveEx = null;  //  Used to control the left front drive wheel
@@ -11,9 +13,25 @@ public class MoveRobot{
     private DcMotorEx rightBackDriveEx = null;  //  Used to control the right back drive wheel
     HardwareMap hwMap = null;
 
+    private IMU imu;
+
     public void initMoveRobot(HardwareMap hwMap){
         this.hwMap = hwMap;
 
+            // Initializing imu
+            imu = hwMap.get(IMU.class, "imu");
+
+            IMU.Parameters parameters = new IMU.Parameters();
+            parameters.angleunit = IMU.AngleUnit.Radians;
+            parameters.accelUnit = IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.RevHubOrientationOnRobot.LogoFacingDirection.UP;
+            parameters.RevHubOrientationOnRobot.UsbFacingDirection.RIGHT;
+
+            imu.initialize(new IMU.Parameters(parameters));
+
+            imu.resetYaw();
+
+            // Mapping motors
             rightFrontDriveEx = hwMap.get(DcMotorEx.class, "Motor_Port_0_CH");
             leftFrontDriveEx = hwMap.get(DcMotorEx.class, "Motor_Port_1_CH");
             leftBackDriveEx = hwMap.get(DcMotorEx.class, "Motor_Port_2_CH");
