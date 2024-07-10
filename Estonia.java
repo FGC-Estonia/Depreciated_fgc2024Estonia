1418d04428c97976be4df92ff7ea745318247c12
Saarmets <65457878+saarmets@users.noreply.github.com>
2024-07-10 19:12:02 +0300

    Update Estonia.java

--- Estonia.java
+++ Estonia.java
@@ -25,13 +25,17 @@ public class Estonia extends LinearOpMode { //file name is Main.java    extends 
          * runMotor.initRunMotor(hardwareMap);
          */
          moveRobot =new MoveRobot();
-         moveRobot.initMoveRobot(hardwareMap, telemetry);
+         moveRobot.initMoveRobot(hardwareMap);
 
         waitForStart();
         
         while (opModeIsActive()) {
-            moveRobot.setSpeed();
-            //runMotor.SetSpeed();
+
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            moveRobot.move(drive, strafe, turn, true);
 
         }
     }
